99666ed9ec46dcece6df56eaf8d19119eaeea10e
dimitkos <33483947+dimitkos@users.noreply.github.com>
2020-04-11 22:53:58 +0300

    Add filters api stracture.

---
+++ MessageHandlers/Filters/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Filters.Controllers
+{
+    [RoutePrefix("values")]
+    public class ValuesController : ApiController
+    {
+        // GET <controller>
+        [HttpGet, Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET <controller>/5
+        [HttpGet, Route("{id:int}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST <controller>
+        [HttpPost, Route("")]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT <controller>/5
+        [HttpPut, Route("{id:int}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE <controller>/5
+        [HttpDelete, Route("{id:int}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
