9bbc259a343a3ba640db44202758f424c50e8d7e
dimitkos <33483947+dimitkos@users.noreply.github.com>
2020-05-03 20:57:12 +0300

    Validate filter.

---
+++ MessageHandlers/Filters/Controllers/ModelsController.cs
@@ -0,0 +1,27 @@
+﻿using Filters.Filters;
+using Filters.Models;
+using Swashbuckle.Swagger.Annotations;
+using System.Net;
+using System.Web.Http;
+
+namespace Filters.Controllers
+{
+    [RoutePrefix("models")]
+    public class ModelsController : ApiController
+    {
+        [HttpPost, Route("object")]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(string))]
+        [ValidateModelState(BodyRequired = true)]    // METHOD 2: Use an Action Filter
+        public IHttpActionResult Post([FromBody]ComplexTypeDto dto)
+        {
+            // METHOD 1: Inline model validation
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            return Ok("Posted data valid");
+        }
+
+    }
+}
--- MessageHandlers/Filters/Models/ComplexTypeDto.cs
+++ MessageHandlers/Filters/Models/ComplexTypeDto.cs
@@ -8,12 +8,12 @@ namespace Filters.Models
 {
     public class ComplexTypeDto
     {
-        //[Required]
-        //[MaxLength(12)]
+        [Required]
+        [MaxLength(12)]
         public string String1 { get; set; }
-        //[RegularExpression(@"^[A-Za-z0-9]{4,12}$")]
+        [RegularExpression(@"^[A-Za-z0-9]{4,12}$")]
         public string String2 { get; set; }
-        //[Range(0, 100)]
+        [Range(0, 100)]
         public int Int1 { get; set; }
         public int Int2 { get; set; }
         public DateTime? Date1 { get; set; }
